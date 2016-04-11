29ee81b014883bf60f3248bd95bea1c7570b6122
keger <1293904605@qq.com>
2016-04-11 17:58:04 +0800

    add stylesheet

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,22 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('/', function(){
+    return view('welcome');
+});
 
-Route::group(['middleware' => ['web']], function () {
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    
+    Route::get('/home', 'HomeController@index');
+
     Route::group(['prefix' => 'tasks'], function() {
         Route::get('/agreement', [
             'as' => 'getAgreement',
@@ -108,6 +100,5 @@ Route::group(['middleware' => ['web']], function () {
             'uses' => 'MethodsController@updateOne'
         ]);
     });
-
 
 });
